585409d82810c609dc14b15a28fd6868787374ae
Melody9-star <79972794+Melody9-star@users.noreply.github.com>
2021-04-23 00:00:48 +0900

    Add files via upload

---
+++ 입력과출력.py
@@ -0,0 +1,7 @@
+number1 = input("첫 번째 문자를 입력하세요 : ")
+number2 = input("두 번째 문자를 입력하세요 : ")
+print(number1+number2)
+
+number3 = int(input("첫 번째 숫자를 입력하세요 : "))
+number4 = int(input("두 번째 숫자를 입력하세요 : "))
+print(number3+number4)
